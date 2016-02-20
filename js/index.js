cd05e78035ef82ad62fe0710bbc3b2e13f4b68ba
Anthony <Anthony@0587339446.wireless.umich.net>
2016-02-20 07:55:49 -0500

    Added marker functionality

--- js/index.js
+++ js/index.js
@@ -1,23 +1,26 @@
 function initMap() {
-    var coordInit = {lat: 42.279594, lng: -83.732124};
-    var userReq = document.getElementById("id_UserReq");
-
-    var map = new google.maps.Map(document.getElementById('id_MapDiv'), {
-        zoom: 15,
-        center: coordInit
-    });
-
-    userReq.addEventListener('click', function(e) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
+        var coordInit = {lat: 42.279594, lng: -83.732124};
+        var userReq = document.getElementById("id_UserReq");
+
+        var map = new google.maps.Map(document.getElementById('id_MapDiv'), {
+            zoom: 17,
+            center: pos
+        });
+        userReq.addEventListener('click', function(e) {
             map.setCenter(pos);
             var marker = new google.maps.Marker({
                 position: pos,
                 map: map,
-                title: 'Hello World!'
+                draggable:true
+            });
+            google.maps.event.addListener(marker, 'dragend', function (evt) {
+                document.getElementById('id_ReqLat').value = evt.latLng.lat().toFixed(3);
+                document.getElementById('id_ReqLng').value = evt.latLng.lng().toFixed(3);
             });
         });
     });
